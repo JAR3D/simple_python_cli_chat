ed9b29cc42f123baaa4ee90faa97927d87177fa8
silvajoa <joao.silva@diconium.com>
2025-04-07 10:41:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# simple_python_cli_chat
---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+
+from openai import OpenAI
+
+load_dotenv()
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_API_BASE_URL = os.environ.get("OPENAI_API_BASE_URL")
+
+client = OpenAI(
+    api_key=OPENAI_API_KEY,
+    base_url=OPENAI_API_BASE_URL,
+)
+
+def get_chat_completion(messages):
+    return client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=1,
+    )
+
+
+prompt = input()
+
+messages = [
+    {
+        "role": "user",
+        "content": prompt
+    },
+    {
+        "role": "system",
+        "content": "You are an helpful assistant for a simple CLI chat. Only respond with text messages. Get creative with the answers!"
+    }
+]
+
+chat_completion = get_chat_completion(messages)
+gpt_response = chat_completion.choices[0].message.content
+
+print(gpt_response)
